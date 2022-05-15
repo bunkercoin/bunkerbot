de157ae40ae7948f2c163ac781cd97a1f88392a8
IdotMaster1 <justapersom@gmail.com>
2022-05-15 22:17:29 +0200

    add skelton

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("i farded")
-exit()
+import discord
+from discord.ext import commands
+
+token = ''
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'My ping is: {round (bot.latency * 1000)} ms')
+
+# Start the bot
+bot.run(token)
