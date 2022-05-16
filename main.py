1c9a5d4376f99af1793244ef36f342121f1275f0
IdotMaster1 <justapersom@gmail.com>
2022-05-16 17:58:10 +0200

    mmmmmyes now usable

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import requests
 import discord
 from discord.ext import commands
 
-# "Settings"
+# 'Settings'
 api = "https://api.bunkercoin.xyz"
-token = ''
+token = 'ODM2NjIzMjQ4Njc0MTI3OTIz.GppQvL.PjQpwRPBKm0uTyl1Po07SDRu0J81c0htCNtmeY'
 
-# Prefix
 bot = commands.Bot(command_prefix='>')
 
 # Get blockcount from the api
@@ -15,6 +14,18 @@ def bkcblockcount():
     parsedblockcount = url.json()
     return parsedblockcount['result']['blockcount']
 
+# Get network hashrate from the api
+def bkcnetworkhashrate():
+    url = requests.get(api + '/hashrate')
+    parsedblockcount = url.json()
+    return parsedblockcount['result']['hashrate']
+
+# Get difficulty from the api
+def bkcdifficulty():
+    url = requests.get(api + '/diff')
+    parsedblockcount = url.json()
+    return parsedblockcount['result']['difficulty']
+    
 # ping command, used to see how sucky my internet is 
 @bot.command()
 async def ping(ctx):
@@ -23,8 +34,10 @@ async def ping(ctx):
 # bunkerinfo commnand, used to see information about bunkercoin, fancy embed!!
 @bot.command()
 async def bunkerinfo(ctx):
-    embed=discord.Embed(title="Bunkercoin info")
-    embed.add_field(name="Blockcount:", value=bkcblockcount(), inline=False)
+    embed=discord.Embed(title="Bunkercoin chain info", color=0x99c1f1)
+    embed.add_field(name="Bunkercoin blockcount", value=bkcblockcount(), inline=False)
+    embed.add_field(name="Bunkercoin network hashrate", value=bkcnetworkhashrate() + " H/s", inline=False)
+    embed.add_field(name="Bunkercoin difficulty", value=bkcdifficulty(), inline=False)
     await ctx.send(embed=embed)
 
 # Start the bot
